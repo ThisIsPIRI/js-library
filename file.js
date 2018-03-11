395a6b1314ba203c5289ee8916f62a6785eaaaba
busyVision <>
2018-03-11 12:26:45 +0900

    Handle different separators

--- file.js
+++ file.js
@@ -8,5 +8,17 @@ const fileReader = {
 			callback(request.responseText);
 		}
 		request.send(null);
+	},
+	/*Replaces all occurrences of replaceThese in data withSeparator
+	and returns the array of all tokens separated withSeparator.
+	replaceThese may have multiple characters; pass them as one concatenated String.
+	If you don't want to replace anything, pass null to replaceThese.
+	If no values are passed, \n will be used for replaceThese and space for withSeparator.*/
+	getTokensFrom : function(data, replaceThese, withSeparator) {
+		replaceThese = replaceThese !== undefined ? replaceThese : '\n'; //No optional parameters; we have to support IEs
+		withSeparator = withSeparator !== undefined ? withSeparator : ' ';
+		if(replaceThese !== null) //Caller didn't want to replace anything
+			data = data.replace(new RegExp('[' + replaceThese + ']', "g"), withSeparator); //replaceThese withSeparator.
+		return data.split(withSeparator).map(function(word) {return word.trim();}); //Separate each words and remove preceding and trailing whitespaces.
 	}
 };
